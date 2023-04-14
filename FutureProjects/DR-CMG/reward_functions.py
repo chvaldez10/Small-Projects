8dbe27f335b4d6fdb96664c29d81348ee901c7cc
chvaldez10 <christianvaldez021@gmail.com>
2023-04-14 11:13:16 -0600

    added folder

---
+++ FutureProjects/DR-CMG/reward_functions.py
@@ -0,0 +1,160 @@
+def reward_function_all_wheels_on_track(params):
+    '''
+    Example of using all_wheels_on_track and speed
+    '''
+
+    # Read input variables
+    all_wheels_on_track = params['all_wheels_on_track']
+    speed = params['speed']
+
+    # Set the speed threshold based your action space
+    SPEED_THRESHOLD = 1.0
+
+    if not all_wheels_on_track:
+        # Penalize if the car goes off track
+        reward = 1e-3
+    elif speed < SPEED_THRESHOLD:
+        # Penalize if the car goes too slow
+        reward = 0.5
+    else:
+        # High reward if the car stays on track and goes fast
+        reward = 1.0
+
+    return float(reward)
+
+def reward_function_default_middle(params):
+    '''
+    Example of rewarding the agent to stay inside the two borders of the track
+    '''
+
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    
+    # Give a very low reward by default
+    reward = 1e-3
+
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+
+    return float(reward)
+
+def reward_function_default_center_line(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3 # likely crashed/ close to off track
+
+    return float(reward)
+
+def reward_function_prevent_zig_zag(params):
+    '''
+    Example of penalize steering, which helps mitigate zig-zag behaviors
+    '''
+
+    # Read input parameters
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
+
+    # Calculate 3 marks that are farther and father away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15 
+    
+    # Penalize reward if the car is steering too much
+    if abs_steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+        
+    return float(reward)
+
+def reward_function_fast_as_heck(params):
+    
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    steering = abs(params['steering_angle'])
+    direction_stearing=params['steering_angle']
+    speed = params['speed']
+    steps = params['steps']
+    progress = params['progress']
+    
+    all_wheels_on_track = params['all_wheels_on_track']
+    ABS_STEERING_THRESHOLD = 15
+    SPEED_THRESHOLD = 5
+    TOTAL_NUM_STEPS = 85
+    # Read input variables
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    reward = 1.0
+    
+    if(all_wheels_on_track == True):
+        reward *= 5.0
+        
+    else:
+        reward -= 5.0
+    if progress == 100:
+        reward += 100
+
+    if not all_wheels_on_track:
+        # Penalize if the car goes off track
+        reward -= 1e-3
+    elif speed < SPEED_THRESHOLD:
+        # Penalize if the car goes too slow
+        reward += 2.20 
+    else:
+        # High reward if the car stays on track and goes fast
+        reward *= 3.0
+
+    # Calculate the direction of the center line based on the closest waypoints
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+    # Convert to degree
+    track_direction = math.degrees(track_direction)
+    # Calculate the difference between the track direction and the heading direction of the car
+    direction_difference = abs(track_direction - heading)
+    # Penalize the reward if the difference is too large
+    DIRECTION_THRESHOLD = 10.0
+    vid=1
+    if direction_difference > DIRECTION_THRESHOLD:
+        vid=1-(direction_difference/50)
+    if vid<0 or vid>1:
+        vid = 0
+        reward *= vid
+    return float(reward)
