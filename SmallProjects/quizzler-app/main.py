a2283a7ff7e59a857e466fdbd2a4c26634a4cd28
Christian Valdez <christianvaldez021@gmail.com>
2023-04-23 10:31:50 -0600

    adding quiz app

---
+++ SmallProjects/quizzler-app/main.py
@@ -0,0 +1,88 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+from data import *
+import json
+
+# question_bank = []
+# for question in question_data:
+#     question_text = question["question"]
+#     question_answer = question["correct_answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+
+# quiz = QuizBrain(question_bank)
+# quiz_ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+################################################################
+#
+#          main function for quiz game
+#
+################################################################
+
+parameters ={
+    "amount": 10,
+    "difficulty": "easy",
+    "type": "boolean",
+}
+
+difficulty = ["Easy", "Medium", "Hard"]
+
+def get_num_of_questions() -> int:
+    try:
+        num_of_questions = input("How many questions do you want?: ")
+        if num_of_questions.lower() == "exit":
+            exit()
+        num_of_questions = int(num_of_questions)
+        return num_of_questions
+    except:
+        print("That is not a valid number")
+        get_num_of_questions()
+
+def get_difficulty() -> str:
+    global difficulty
+
+    diff = input("What difficulty do you want?: ")
+
+    if diff.lower() == "exit":
+        exit()
+    elif diff.title() not in difficulty:
+        print("Not a valid difficulty")
+        return get_difficulty()
+    
+    return diff.lower()
+
+
+def fill_parameters() -> None:
+    global parameters
+
+    parameters["amount"] = get_num_of_questions()
+    parameters["difficulty"] = get_difficulty()
+
+    paramas_pretty = json.dumps(parameters, indent=4)
+    print(f"\nParameters chosen:\n\n{paramas_pretty}\n")
+
+def play_quiz_game() -> None:
+    global parameters
+
+    question_data = get_data_from_api(parameters)
+
+    question_bank = []
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
+
+    # quiz = QuizBrain(question_bank)
+    # quiz_ui = QuizInterface(quiz)
+
+fill_parameters()    
+# play_quiz_game()
---
+++ SmallProjects/quizzler-app/ui.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quiz")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label =Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_button_clicked)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_button_clicked)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_qestion()
+
+        self.window.mainloop()
+
+    def get_next_qestion(self):
+        self.canvas.configure(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.configure(text="Score: " + str(self.quiz.score))
+            question_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=question_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="End of Quiz")
+            self.true_button.configure(state="disabled")
+            self.false_button.configure(state="disabled")
+
+    def true_button_clicked(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_button_clicked(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.configure(bg="green")
+        else:
+            self.canvas.configure(bg="red")
+        
+        self.window.after(1000, self.get_next_qestion)
+
+        
