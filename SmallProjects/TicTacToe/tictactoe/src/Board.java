e07d4af5b0c002d6c5c3cbd08c442124b170d6aa
chvaldez10 <christianvaldez021@gmail.com>
2023-06-20 17:22:53 -0600

    added more methods to Board

--- SmallProjects/TicTacToe/tictactoe/src/Board.java
+++ SmallProjects/TicTacToe/tictactoe/src/Board.java
@@ -28,6 +28,13 @@ public class Board implements Constants{
             return false;
     }
 
+    public boolean oWins() {
+        if(checkWinner(letterO)==1)
+            return true;
+        else
+            return false;
+    }
+
     public int checkWinner(char mark) {
         //check rows
         for (int row=0; row<3; row++) {
@@ -46,5 +53,67 @@ public class Board implements Constants{
             return 1;
 
         return 0;
+    }
+
+    public void addMark(int row, int col, char mark) {
+        tictacBoard[row][col] = mark;
+        markCount++;
+    }
+
+    /**
+     * displays the game board to the console.
+     */
+    public void display() {
+        displayColumnHeaders();
+        addHyphens();
+        for(int row=0; row<3; row++) {
+            addSpaces();
+            System.out.println("    row " + row + " ");
+            for (int col = 0; col < 3; col++)
+                System.out.print("|  " + getMark(row, col) + "  ");
+            System.out.println("|");
+            addSpaces();
+            addHyphens();
+        }
+    }
+
+    /**
+     * clears game board
+     */
+    public void clear() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                tictacBoard[i][j] = spaceChar;
+        markCount = 0;
+    }
+
+    /**
+     * displays column numbers on game board
+     */
+    void displayColumnHeaders() {
+        System.out.print("          ");
+        for (int j = 0; j < 3; j++)
+            System.out.print("|col " + j);
+        System.out.println();
+    }
+
+    /**
+     * row separators
+     */
+    void addHyphens() {
+        System.out.print("          ");
+        for (int j = 0; j < 3; j++)
+            System.out.print("+-----");
+        System.out.println("+");
+    }
+
+    /**
+     * column separators
+     */
+    void addSpaces() {
+        System.out.print("          ");
+        for (int j = 0; j < 3; j++)
+            System.out.print("|     ");
+        System.out.println("|");
     }
 }
