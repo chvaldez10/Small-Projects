5a65510b1db6a5760839db8137da82524eca585c
chvaldez10 <christianvaldez021@gmail.com>
2024-01-14 20:39:24 -0700

    finished live wallpaper

--- LiveWallpaper/from_moviepy.py
+++ LiveWallpaper/from_moviepy.py
@@ -2,26 +2,45 @@ import os
 import traceback
 import ctypes
 import time
+import cv2
 
-SPI_SETDESKWALLPAPER = 20
+SPI_SETDESKWALLPAPER = 20 # action number to set wallpaper
 VIDEO_PATH_PREFIX = r"./assets/video"
+IMAGE_PATH_PREFIX = r"./assets/frames"
 
 def set_wallpaper(image_path):
-    """
-    Set the desktop wallpaper to the specified image.
-
-    Args:
-    image_path (str): The path to the image file.
-    """
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+    print(f"Setting wallpaper: {image_path}") 
+    result = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+    if not result:
+        print(f"Error setting wallpaper. Last error: {ctypes.GetLastError()}") 
 
 def read_video_frames(video_path):
-    pass
+    cap = cv2.VideoCapture(video_path)
 
-def wallpaper_generator(video_path, frame_rate):
-    for frame in read_video_frames(video_path):
-        yield frame
-        time.sleep(1/frame_rate)
+    if not cap.isOpened():
+        raise IOError("Cannot open video file")
+
+    frame_count = 0
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        frame_path = os.path.join(IMAGE_PATH_PREFIX, f'frame_{frame_count}.jpg')
+        cv2.imwrite(frame_path, frame)
+
+        frame_count += 1
+
+    cap.release()
+    return frame_count  # Return the total number of frames saved
+
+def set_wallpapers_from_saved_frames(total_frames, frame_rate):
+    for frame_count in range(total_frames):
+        frame_path = os.path.join(IMAGE_PATH_PREFIX, f'frame_{frame_count}.jpg')
+        set_wallpaper(frame_path)
+        time.sleep(1 / frame_rate)
 
 ###########################################################
 #
@@ -34,11 +53,8 @@ def main() -> None:
     video_path = os.path.join(VIDEO_PATH_PREFIX, video_filename)
     frame_rate = 30
 
-    while True:
-        for frame in read_video_frames(video_path):
-            set_wallpaper(frame)
-            time.sleep(1 / frame_rate)
-
+    total_frames = read_video_frames(video_path)
+    set_wallpapers_from_saved_frames(total_frames, frame_rate)
 
 if __name__ == "__main__":
     try:
