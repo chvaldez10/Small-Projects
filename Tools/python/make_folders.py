6b6115cbca5c1db9f1f8c11a6bba415262d1fd52
chvaldez10 <christianvaldez021@gmail.com>
2024-04-24 20:53:50 -0600

    created script to generate folders

--- Tools/python/make_folders.py
+++ Tools/python/make_folders.py
@@ -31,14 +31,27 @@ def print_args(args: argparse.Namespace) -> None:
         print(f"{arg_name}: {arg_value}")
 
 def generate_folders(args: argparse.Namespace) -> None:
-    folder_prefix = args.folder_prefix
-    max = args.num_of_iterations
-    root_dir = args.root_dir
-    counter = 0
+    """
+        Given a prefix, number of iterations, and a root directory.
     
-    return
-    while counter < max:
-        pass
+        Generate folders n items where n is the number of iterations.
+    """
+    folder_prefix = args.folder_prefix
+    num_of_iterations = args.num_of_iterations
+    root_dir = args.root_dir
+    
+    for counter in range(num_of_iterations):
+        folder_name = folder_prefix + "_" + str(counter)
+        folder_path = os.path.join(root_dir, folder_name)
+        
+        try:
+            if not os.path.exists(folder_path):
+                print(f"Generating {folder_path}")
+                os.makedirs(folder_path)
+            else:
+                print(f"Failed to generate {folder_path}")
+        except Exception as e:
+            print(f"Error: {e}")
 
 def main(args: argparse.Namespace) -> None:
     """Main function that prints all arguments provided to the script."""
